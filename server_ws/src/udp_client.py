d8e44087bb12b70fa2a38e3ed08d61b11e507ad2
YuZhong-Chen <yuzhong1214@gmail.com>
2023-12-27 23:49:06 +0800

    Rewrite udp client code.

--- server_ws/src/udp_client.py
+++ server_ws/src/udp_client.py
@@ -3,25 +3,34 @@
 import socket
 
 
-def client_program():
+def UDP_Client(data):
+    # Set up the host and port
     host = "192.168.1.103"
-    port = 5000  # socket server port number
+    port = 5000
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    # Get instance
+    client_socket = socket.socket()
 
-    message = input(" -> ")  # take input
+    # Connect to the server
+    client_socket.connect((host, port))
 
-    while message.lower().strip() != "bye":
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+    # Send message to server
+    client_socket.send(data.encode())
 
-        print("Received from server: " + data)  # show in terminal
+    # Receive response
+    data = client_socket.recv(1024).decode()
+    print("Received from server: " + data)
 
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
+    # Close the connection
+    client_socket.close()
 
 
 if __name__ == "__main__":
-    client_program()
+    # Take input from command line
+    message = input(" -> ")
+
+    while True:
+        UDP_Client(message)
+
+        # Take input again
+        message = input(" -> ")
