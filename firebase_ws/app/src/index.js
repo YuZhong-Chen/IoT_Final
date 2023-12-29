a813abf479a0d59c5dac541b4c3169f8d7e0e1fb
YuZhong-Chen <yuzhong1214@gmail.com>
2023-12-29 17:41:43 +0800

    Update data sort.

--- firebase_ws/app/src/index.js
+++ firebase_ws/app/src/index.js
@@ -2,66 +2,51 @@ import Chart from "../node_modules/chart.js/auto";
 import './index.css';
 
 var data1 = [
-    { "Time": "05:00", "Volume": 1017 },
-    { "Time": "06:00", "Volume": 1066 },
-    { "Time": "07:00", "Volume": 1475 },
-    { "Time": "08:00", "Volume": 1270 },
-    { "Time": "09:00", "Volume": 1496 },
-    { "Time": "10:00", "Volume": 1712 },
-    { "Time": "11:00", "Volume": 1068 },
-    { "Time": "12:00", "Volume": 1018 },
-    { "Time": "13:00", "Volume": 1283 },
-    { "Time": "13:30", "Volume": NaN },
-    { "Time": "13:00", "Volume": 1659 },
-    { "Time": "15:00", "Volume": 1050 },
-    { "Time": "16:00", "Volume": 1200 },
-    { "Time": "10:00", "Volume": 1138 },
-    { "Time": "18:00", "Volume": 1386 },
-    { "Time": "10:01", "Volume": 1041 },
-    { "Time": "20:00", "Volume": 1734 },
-    { "Time": "21:00", "Volume": 1372 },
-    { "Time": "22:00", "Volume": 1270 },
-    { "Time": "21:00", "Volume": NaN },
-    { "Time": "25:00", "Volume": 1300 }];
+    { "Time": "15:30:00", "Volume": NaN },
+    { "Time": "13:00:00", "Volume": 1659 },
+    { "Time": "15:00:00", "Volume": 1050 },
+    { "Time": "16:00:00", "Volume": 1200 },
+    { "Time": "10:00:00", "Volume": 1138 },
+    { "Time": "18:00:00", "Volume": 1386 },
+    { "Time": "10:01:00", "Volume": 1041 },
+    { "Time": "20:00:00", "Volume": 1734 },
+    { "Time": "20:00:01", "Volume": 1372 },
+    { "Time": "22:00:00", "Volume": 1270 },
+    { "Time": "21:00:00", "Volume": NaN },];
 
 var data2 = [
-    { "Time": "05:00", "Volume": 1017 },
-    { "Time": "06:00", "Volume": 1066 },
-    { "Time": "07:00", "Volume": 1475 },
-    { "Time": "08:00", "Volume": 1270 },
-    { "Time": "09:00", "Volume": 1496 },
-    { "Time": "10:00", "Volume": 1712 },
-    { "Time": "11:00", "Volume": 1068 },
-    { "Time": "12:00", "Volume": 1018 },
-    { "Time": "13:00", "Volume": 1283 },
-    { "Time": "13:30", "Volume": NaN },
-    { "Time": "13:00", "Volume": 1659 },
-    { "Time": "15:00", "Volume": 1050 },
-    { "Time": "16:00", "Volume": 1200 },
-    { "Time": "10:00", "Volume": 1138 },
-    { "Time": "18:00", "Volume": 1386 },
-    { "Time": "10:01", "Volume": 1041 },
-    { "Time": "20:00", "Volume": 1734 },
-    { "Time": "21:00", "Volume": 1372 },
-    { "Time": "22:00", "Volume": 1270 },
-    { "Time": "21:00", "Volume": NaN },
-    { "Time": "25:00", "Volume": 1300 }];
+    { "Time": "05:00:00", "Volume": 1017 },
+    { "Time": "06:00:00", "Volume": 1066 },
+    { "Time": "07:00:00", "Volume": 1475 },
+    { "Time": "08:00:00", "Volume": 1270 },
+    { "Time": "09:00:00", "Volume": 1496 },
+    { "Time": "10:00:00", "Volume": 1712 },
+    { "Time": "11:00:00", "Volume": 1068 },
+    { "Time": "12:00:00", "Volume": 1018 },
+    { "Time": "13:00:00", "Volume": 1283 },
+    { "Time": "13:30:00", "Volume": NaN },
+    { "Time": "18:00:00", "Volume": 1386 },
+    { "Time": "10:01:00", "Volume": 1041 },
+    { "Time": "21:00:00", "Volume": 1372 },
+    { "Time": "22:00:00", "Volume": 1270 },
+    { "Time": "21:00:00", "Volume": NaN }];
 
 // Sort by time
 data1.sort((a, b) => {
-    var aTime = a.Time.split(':');
-    var bTime = b.Time.split(':');
-    return (aTime[0] * 60 + aTime[1]) - (bTime[0] * 60 + bTime[1]);
+    var aTime = a.Time.split(":");
+    var bTime = b.Time.split(":");
+    return (aTime[0] * 3600 + aTime[1] * 60 + aTime[2]) - (bTime[0] * 3600 + bTime[1] * 60 + bTime[2]);
 });
 data2.sort((a, b) => {
-    var aTime = a.Time.split(':');
-    var bTime = b.Time.split(':');
-    return (aTime[0] * 60 + aTime[1]) - (bTime[0] * 60 + bTime[1]);
+    var aTime = a.Time.split(":");
+    var bTime = b.Time.split(":");
+    return (aTime[0] * 3600 + aTime[1] * 60 + aTime[2]) - (bTime[0] * 3600 + bTime[1] * 60 + bTime[2]);
 });
 
-// Select the last 20 data
-data1 = data1.slice(-20);
-data2 = data1.slice(-20);
+// Select the last data
+const last_data = 15;
+data1 = data1.slice(-last_data);
+data2 = data2.slice(-last_data);
 
 // Get element
 var ctx1 = document.getElementById("Chart1").getContext("2d");
